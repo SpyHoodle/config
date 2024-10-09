f23efee17f6b7832b7b9bc08d6e35c433a2def19
Maddie H <maddie@spyhoodle.me>
2024-10-09 23:17:01 +0100

    feat: change font and enable zsh integration for wezterm

--- homes/common/wezterm.nix
+++ homes/common/wezterm.nix
@@ -1,9 +1,12 @@
 {
   programs.wezterm = {
     enable = true;
+    enableZshIntegration = true;
     extraConfig = ''
       return {
         front_end = 'WebGpu',
+        font = wezterm.font 'Iosevka Nerd Font',
+        font_size = 16,
         color_scheme = 'OneDark (base16)',
         hide_tab_bar_if_only_one_tab = true,
         native_macos_fullscreen_mode = true,
