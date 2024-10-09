4ab3b7b6d2dc5ef2863fb66b6f5d282c60eac7ae
Maddie H <maddie@spyhoodle.me>
2024-10-09 22:30:05 +0100

    fix: use native macos fullscreen on wezterm to fix menubar hiding

--- homes/common/wezterm.nix
+++ homes/common/wezterm.nix
@@ -6,6 +6,7 @@
         front_end = 'WebGpu',
         color_scheme = 'OneDark (base16)',
         hide_tab_bar_if_only_one_tab = true,
+        native_macos_fullscreen_mode = true,
         window_close_confirmation = 'NeverPrompt',
         window_padding = {
           left = 0,
