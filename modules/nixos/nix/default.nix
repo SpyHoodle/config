e58a162b9b047ac18f56747c4345eb66d7a1009d
Madeleine Holbrook <maddie@spyhoodle.me>
2025-03-07 18:17:30 +0000

    Add hyprland cachix to prevent dependencies from being built

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -16,10 +16,11 @@
       builders-use-substitutes = lib.mkIf config.host.nix.caches.enable true;
 
       substituters = lib.mkIf config.host.nix.caches.enable [
-        "https://anyrun.cachix.org"
-        "https://cache.lix.systems"
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
+        "https://anyrun.cachix.org/"
+        "https://cache.lix.systems/"
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org/"
+        "https://hyprland.cachix.org/"
       ];
 
       trusted-public-keys = lib.mkIf config.host.nix.caches.enable [
@@ -27,6 +28,7 @@
         "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
 
       experimental-features = lib.mkIf config.host.nix.experimentalFeatures.enable [
