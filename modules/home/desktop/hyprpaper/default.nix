c46df22963579dd3856963159893a25e0beda5dc
Madeleine Holbrook <maddie@spyhoodle.me>
2026-01-31 19:01:06 +0000

    hyprpaper: simplify wallpaper configuration with global monitor fallback

--- modules/home/desktop/hyprpaper/default.nix
+++ modules/home/desktop/hyprpaper/default.nix
@@ -18,17 +18,12 @@
       enable = true;
       settings = {
         splash = false;
-        preload = [ "${config.host.theme.wallpaper}" ];
-        wallpaper =
-          if config.host.desktop.hyprland.monitors != [ ] then
-            builtins.map (m:
-              let
-                monitorName = builtins.head (lib.splitString "," m);
-              in
-              "${monitorName},${config.host.theme.wallpaper}"
-            ) config.host.desktop.hyprland.monitors
-          else
-            [ ",${config.host.theme.wallpaper}" ];
+        wallpaper = [
+          {
+            monitor = "";
+            path = "${config.host.theme.wallpaper}";
+          }
+        ];
       };
     };
   };
