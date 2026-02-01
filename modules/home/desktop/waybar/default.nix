69c3ca86b3e639ce1066eda01faf05473396e66e
Madeleine Holbrook <maddie@spyhoodle.me>
2026-02-01 18:16:52 +0000

    fix(waybar): theme calendar colors using color palette
    Replace hardcoded hex colors in calendar widget with theme palette references:
    - months: base0A (yellow)
    - days: base05 (text)
    - weeks: base0B (green)
    - weekdays: base09 (orange)
    - today: accent color

--- modules/home/desktop/waybar/default.nix
+++ modules/home/desktop/waybar/default.nix
@@ -103,11 +103,11 @@
               "weeks-pos" = "right";
               "on-scroll" = 1;
               "format" = {
-                "months" = "<span color='#ffead3'><b>{}</b></span>";
-                "days" = "<span color='#ecc6d9'><b>{}</b></span>";
-                "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
-                "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
-                "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
+                "months" = "<span color='#${config.host.theme.colors.pallete.base0A.hex}'><b>{}</b></span>";
+                "days" = "<span color='#${config.host.theme.colors.pallete.base05.hex}'><b>{}</b></span>";
+                "weeks" = "<span color='#${config.host.theme.colors.pallete.base0B.hex}'><b>W{}</b></span>";
+                "weekdays" = "<span color='#${config.host.theme.colors.pallete.base09.hex}'><b>{}</b></span>";
+                "today" = "<span color='#${config.host.theme.colors.pallete.${config.host.theme.colors.accent}.hex}'><b><u>{}</u></b></span>";
               };
             };
             "actions" = {
